c3ca24a32cdc7c81257b745e05dd79eac03b84d1
rahdnus <rahdnus119@gmail.com>
2022-05-06 15:20:04 +0530

    Added Item

---
+++ src/Scripts/Runtime/Item.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DokkaebiBag.Generic{
+public enum Stackable{X0, X16, X32, X64}
+public enum ItemType{Weapon,Accessory,Armor,Consumable}
+public class Item : MonoBehaviour
+{
+   [SerializeField] private string id;
+    public string _ID{
+        get
+        {
+            if(id==null)
+            {
+                Debug.LogError("No ID assigned to "+ gameObject.name);
+                return null;
+            }
+            return id;
+        }
+        set{}
+    }
+    [SerializeField] private string description;
+    public string _Description{
+        get
+        {
+            if(description==null)
+            {
+                Debug.LogError("No description assigned to "+ gameObject.name);
+                return null;
+            }
+            return description;
+        }
+        set{}
+    }
+    [SerializeField] private Stackable stack;
+    public Stackable _Stack{
+        get
+        {
+            return stack;
+        }
+        set{}
+    }
+    [SerializeField] private ItemType itype;
+    public ItemType _ItemType{
+        get
+        {
+            return itype;
+        }
+        set{}
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+
+    }
+}
+}
--- src/Scripts/Runtime/UI/ItemView.cs
+++ src/Scripts/Runtime/UI/ItemView.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+namespace DokkaebiBag.UI{
 
-public class Item : MonoBehaviour
+public class ItemView : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -15,4 +16,5 @@ public class Item : MonoBehaviour
     {
         
     }
+}
 }
